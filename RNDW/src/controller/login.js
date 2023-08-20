bc837e79a2312e7b4f20549e402e335ff81d639c
Prashant Rai <19ce073@gardividyapith.ac.in>
2023-08-20 16:11:42 +0530

    Backend Completed Login-Signup using MVC architecture

---
+++ RNDW/src/controller/login.js
@@ -0,0 +1,67 @@
+const signupModel = require("../models/schema");
+
+
+async function signupHandler (req, res) {
+    try {
+
+        // console.log("Request Body:", req.body);
+        const {name, email, phone, password} = req.body;
+
+        if(!name || !email || !phone || !password){
+            return res.status(400).json({ success: false, message: "All fields are Required"});
+        }
+
+        const availabledata = await signupModel.findOne({email});
+        // console.log("14",availabledata)
+        if(availabledata){
+            return res.status(409).json({ success: false, message: "Email or Phone is Already Registered"});
+        }
+
+        const newUser = new signupModel({name, email, phone, password});
+        const savedUser = await newUser.save();
+
+        return res.status(200).json({ success: true, message: "Registration Success", savedUser});
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ success: false, error: error.message });
+    }
+}
+
+
+async function getdataHandler (req, res) {
+    try {
+        const data = await signupModel.find()
+        console.log("36",data);
+        return res.json({success:true, data})
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, error: error.message });
+    }
+}
+
+async function loginHandler (req, res) {
+    try {
+        // console.log(req.body);
+        const {email, phone, password} = req.body
+        if((!email || !phone) && !password){
+            return res.status(400).json({ success: false, message: "All  fields are required"});
+        }
+
+        const user = await signupModel.findOne({$or: [{email}, {phone}]});
+        // console.log("51",user)
+        if (!user) {
+            return res.json({success:false, message: "User Not Found, Kindly Register"});
+        }
+
+        if (user.password !== password){
+            return res.status(200).json({success:false, message: "Incorrect Password"});
+        }
+
+        return res.status(200).json({success:true, message: "Login Successful"});
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, error: error.message });
+    }
+}
+
+module.exports = {loginHandler, getdataHandler, signupHandler};
--- RNDW/src/routes/login.js
+++ RNDW/src/routes/login.js
@@ -1,41 +1,14 @@
 const express = require("express");
 const LoginRouter = express.Router();
 const signupModel = require("../models/schema");
+const {loginHandler, getdataHandler, signupHandler} = require("../controller/login")
 
-LoginRouter.post("/signup", async (req, res) => {
-    try {
+LoginRouter.post("/signup", signupHandler);
 
-        // console.log("Request Body:", req.body);
-        const {name, email, phone, password} = req.body;
+LoginRouter.get("/getdata", getdataHandler);
 
-        if(!name || !email || !phone || !password){
-            return res.status(400).json({ success: false, message: "All fields are Required"});
-        }
-
-        const availabledata = await signupModel.findOne({email});
-        // console.log("14",availabledata)
-        if(availabledata){
-            return res.status(409).json({ success: false, message: "Email or Phone is Already Registered"});
-        }
-
-        const newUser = new signupModel({name, email, phone, password});
-        const savedUser = await newUser.save();
-
-        return res.status(200).json({ success: true, message: "Registration Success", savedUser});
+LoginRouter.post("/login", loginHandler);
 
 
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ success: false, error: error.message });
-    }
-});
-
-LoginRouter.post("/login", (req, res) => {
-    try {
-        console.log(req.body);
-    } catch (error) {
-        console.log(error);
-    }
-});
 
 module.exports = LoginRouter;
