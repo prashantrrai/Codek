2944de3989d91ffea1a367b0190b217f4cf6c548
Prashant Rai <19ce073@gardividyapith.ac.in>
2023-10-09 01:31:41 +0530

    Vytech Enterprise Task done

---
+++ Vytech Enterprise/index.js
@@ -0,0 +1,51 @@
+function showMessage() {
+    let paragraph = document.getElementById('para').value
+    // console.log(paragraph)
+
+    const result = findLongestAndShortestWords(paragraph);
+    // console.log(result)
+    const rev_result = reverseWords(result)
+
+    console.log("Longest Word:", result.longest, "Longest Reverse:", rev_result.reverseL1 );
+    console.log("Shortest Word:", result.shortest, "Shortest Reverse:", rev_result.reverseS1 );
+
+}
+
+function findLongestAndShortestWords(paragraph) {
+    const words = paragraph.split(' ');
+
+    let longestWord = '';
+    let shortestWord = '';
+
+    for (const word of words){
+        const cleanedWord  = word.replace(/[.,!?\s]/g, '');
+        // console.log(cleanedWord, cleanedWord.length)
+        if(cleanedWord.length > longestWord.length){
+            longestWord = cleanedWord;
+        }
+
+        if(shortestWord === '' || cleanedWord.length < shortestWord.length){
+            shortestWord = cleanedWord;
+        }
+    }
+    return { longest: longestWord, shortest: shortestWord };
+}
+
+function reverseWords(result){
+    const longestReverse = result.longest.split('').reverse().join('')
+    const shortestReverse = result.shortest.split('').reverse().join('')
+    return { reverseL1: longestReverse, reverseS1: shortestReverse}
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    var myButton = document.getElementById('submitBtn');
+    myButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        showMessage();
+    }); 
+});
+
